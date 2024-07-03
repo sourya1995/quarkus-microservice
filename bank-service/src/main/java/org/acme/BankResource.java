1a9717cc0ac3a264085d1ef7ccc27d41e84b505c
Sourya Bhattacharya <souryabhattacharya21@gmail.com>
2024-07-03 17:21:04 +0530

    jakarta.ws.rs.Produces

--- bank-service/src/main/java/org/acme/BankResource.java
+++ bank-service/src/main/java/org/acme/BankResource.java
@@ -7,10 +7,11 @@ import java.util.Optional;
 import org.eclipse.microprofile.config.inject.ConfigProperties;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import jakarta.enterprise.inject.Produces;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/bank")
